5a14fd0ebfb360ac2ff22a65e98c57e5cf041dbb
Yibo Jiang <yibo.jiang@coconutislandstudio.com>
2014-10-18 00:48:48 +0800

    update character

---
+++ Assets/Game/Scripts/Manager/GameManager.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+private static var instance : GameManager;
+ 
+public static function Instance() : GameManager
+{
+    if (instance == null)
+        instance =GameObject.FindObjectOfType(GameManager) as GameManager;
+    return instance;
+}
+
+
+var hpPrefab:GameObject;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ Assets/Game/Scripts/Object/HPContainer.js
@@ -0,0 +1,25 @@
+﻿#pragma strict
+
+import System.Collections.Generic;
+
+var hpbars:List.<GameObject>;
+
+function UpdateLayout(){
+	var i:int;
+	for (i=0;i<hpbars.Count;i++){
+		hpbars[i].transform.localPosition.x=0;
+		hpbars[i].transform.localPosition.y=i*0.3;
+	}
+}
+
+function AddHPObj(_obj:GameObject){
+	hpbars.Add(_obj);
+	_obj.transform.parent=transform;
+	UpdateLayout();
+}
+
+function RemoveHPObj(_obj:GameObject){
+	hpbars.Remove(_obj);
+	_obj.transform.parent=transform;
+	UpdateLayout();
+}
