61327ec69eb15f9e9999f0fde5544891f3aa7599
Yibo Jiang <yibo.jiang@coconutislandstudio.com>
2014-10-18 09:24:47 +0800

    bababbvava

--- Assets/Game/Scripts/Manager/GameManager.js
+++ Assets/Game/Scripts/Manager/GameManager.js
@@ -13,3 +13,23 @@ var hpPrefab:GameObject;
 
 var characters:List.<Character>;
 
+var mobs:GameObject[];
+
+var mobToggle:float;
+var mobInterval:float=2;
+
+
+function Update(){
+
+	mobToggle+=Time.deltaTime;
+	if (mobToggle<mobInterval){
+		
+	}
+	else{
+		mobToggle-=mobInterval;
+		var generatePos:Vector3=Vector3(Random.Range(444,485),12,-286 );
+		Instantiate(mobs[Random.Range(0,mobs.Length)],generatePos,Quaternion.identity);	
+	}
+	
+
+}
