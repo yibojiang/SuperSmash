aea0ae03fa9bbd7e9735eff388233f95c335a4ce
neo <neo@coconutislandstudio.com>
2014-10-21 20:42:14 +0800

    balance

--- Assets/Game/Scripts/Object/Spitter.js
+++ Assets/Game/Scripts/Object/Spitter.js
@@ -2,6 +2,8 @@
 var spitPrefab:GameObject;
 var shotTransform:Transform;
 
+var throwForce:Vector3;
+
 function Spit(){
 	var sp:SpitBullet=Instantiate(spitPrefab,shotTransform.position,Quaternion.identity).GetComponent(SpitBullet) as SpitBullet;
 
@@ -10,7 +12,7 @@ function Spit(){
 	sp.character=bp.character;
 
 	if (bp.character!=null){
-		sp.gameObject.rigidbody.AddForce(Vector3(bp.character.flipDir*600,0,0) );
+		sp.gameObject.rigidbody.AddForce(Vector3(bp.character.flipDir*throwForce.x,throwForce.y,0) );
 		if (bp.character.flipDir>0){
 			sp.transform.eulerAngles.y=0;
 		}
