bcbfc7ee0aa414d22aff38cf8c7716fce51a63a5
Yibo Jiang <yibo.jiang@coconutislandstudio.com>
2014-11-03 22:56:25 +0800

    tweak basket

---
+++ Assets/Game/Scripts/Object/Apple.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function OnTriggerEnter(_other:Collider){
+	if (_other.CompareTag("Basket")){
+		Debug.Log("goal");
+	}
+	
+}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
